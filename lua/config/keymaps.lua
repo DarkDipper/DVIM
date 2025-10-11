b8f10baef7bff439e8d4c4346d623be2da6ab32e
Phong HT - SWE <phonght@cnv.vn>
2025-10-11 12:38:08 +0700

    fix dotnet and java

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,15 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
-
--- Custom for teletescope
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "TelescopeResults",
-  callback = function(ctx)
-    vim.api.nvim_buf_call(ctx.buf, function()
-      vim.fn.matchadd("TelescopeParent", "\t\t.*$")
-      vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
-    end)
-  end,
-})
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "roslyn",
+        "rzls",
+      },
+    },
+  },
+}
--- lua/plugins/mason-workaround.lua
+++ lua/plugins/mason-workaround.lua
@@ -1,4 +1,9 @@
 return {
-   { "mason-org/mason.nvim", version = "1.11.0" },
-   { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+   "mason-org/mason.nvim",
+   opts = {
+      registries = {
+         "github:mason-org/mason-registry",
+         "github:Crashdummyy/mason-registry",
+      },
+   },
 }
